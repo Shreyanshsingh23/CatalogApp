55f0f0a78d35341e567b5e177a99d0a355768811
Shreyanshsingh23 <shreyanshrewa@gmail.com>
2024-07-05 10:29:53 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp()); // 
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {  //build method bht important hai hamare liye kyuki
+    return const Placeholder();
+  }
+}
